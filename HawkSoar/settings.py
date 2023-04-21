5a057c98dc60d3b9d152e42fce8fd7dd95ffcd16
ez98 <ezacarias@students.prairiestate.edu>
2023-04-21 08:38:39 -0500

    READ README

--- HawkSoar/settings.py
+++ HawkSoar/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'daphne',
     'channels',
+    'django_filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
